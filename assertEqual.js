2466739915af11034b418fb893f09eaf58f5aa19
dantan123 <dtan9988@gmail.com>
2021-01-05 00:12:42 +0000

    write an assert function to check code correctness

---
+++ assertEqual.js
@@ -0,0 +1,27 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  let formatExpected;
+  let formatActual;
+  if (typeof actual === 'string') {
+    formatActual = `"${actual}"`;
+  } else {
+    formatActual = `${actual}`;
+  }
+  if (typeof expected === 'string') {
+    formatExpected = `"${expected}"`;
+  } else {
+    formatExpected = `${expected}`;
+  }
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${formatActual} === ${formatExpected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${formatActual} !== ${formatExpected}`);
+  }
+}
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("Lighthouse Labs", "Lighthouse" + " " + "Labs");
+assertEqual(1, 1);
+assertEqual(3, 5);
+assertEqual(222, 111 + 111);
