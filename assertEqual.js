2bae14aba93c5adebef4335f74a73da851ea9f17
dantan123 <dtan9988@gmail.com>
2021-01-05 00:52:23 +0000

    check multiple strings with template literal

--- assertEqual.js
+++ assertEqual.js
@@ -1,18 +1,37 @@
-// FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   let formatExpected;
   let formatActual;
-  if (typeof actual === 'string') {
-    formatActual = `"${actual}"`;
+
+  if (typeof actual === 'string' && typeof expected === 'string') {
+    formatExpected = ' ';
+    formatActual = ' ';
+    
+    let expectedArray = expected.split(' ');
+    let actualArray = actual.split(' ');
+
+    console.log(expectedArray, actualArray);
+
+    if (expectedArray.length > 1) {
+      for (let expected of expectedArray) {
+        formatExpected += expected + ' ';
+      }
+    } else {
+      formatExpected = expected;
+    }
+
+    if (actualArray.length > 1) {
+      for (let actual of actualArray) {
+        formatActual += actual + ' ';
+      }
+    } else {
+      formatActual = actual;
+    }
   } else {
-    formatActual = `${actual}`;
+    formatExpected = expected;
+    formatActual = actual;
   }
-  if (typeof expected === 'string') {
-    formatExpected = `"${expected}"`;
-  } else {
-    formatExpected = `${expected}`;
-  }
-  if (actual === expected) {
+
+  if (formatActual === formatExpected) {
     console.log(`✅✅✅ Assertion Passed: ${formatActual} === ${formatExpected}`);
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${formatActual} !== ${formatExpected}`);
