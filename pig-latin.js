0fbce9337710ba0f4c6d6e5e7fc62b3cc858ba94
dantan123 <dtan9988@gmail.com>
2021-01-05 03:00:13 +0000

    string manipulation given command inputs

---
+++ pig-latin.js
@@ -0,0 +1,18 @@
+const pigLatin = function(args) {
+  if (args.length <= 2) {
+    console.log('Not enough arguments');
+    return;
+  }
+  let stringArray = args.slice(2);
+  let result = '';
+
+  for (let string of stringArray) {
+    let newString = string.slice(1) + string[0] + 'ay';
+    result += newString + ' ';
+  }
+
+  return result;
+};
+
+const args = process.argv;
+console.log(pigLatin(args));
