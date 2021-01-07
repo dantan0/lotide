b502d0e802812d23bc4285f2081941ef6ec84030
dantan123 <dtan9988@gmail.com>
2021-01-07 02:01:35 +0000

    count the freq of letters in a string

---
+++ countLetters.js
@@ -0,0 +1,40 @@
+const assertEqual = function(actual, expected) {
+  let formatExpected = ' ';
+  let formatActual = ' ';
+
+  if (typeof actual === 'string') {
+    formatActual = `"${actual}"`;
+  } else {
+    formatActual = `${actual}`;
+  }
+  
+  if (typeof expected === 'string') {
+    formatExpected = `"${expected}"`;
+  } else {
+    formatExpected = `${expected}`;
+  }
+
+  if (formatActual === formatExpected) {
+    console.log(`✅✅✅ Assertion Passed: ${formatActual} === ${formatExpected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${formatActual} !== ${formatExpected}`);
+  }
+};
+
+const countLetters = function(str) {
+  let noSpace = str.split(' ').join('');
+  let result = {};
+  for (let letter of noSpace) {
+    if (result[letter]) {
+      result[letter] += 1;
+    } else {
+      result[letter] = 1;
+    }
+  }
+  return result;
+};
+
+let res = countLetters('lighthouse in the house');
+console.log(res);
+assertEqual(res['l'], 1);
+assertEqual(res['h'], 4);
---
+++ countOnly.js
@@ -0,0 +1,58 @@
+const assertEqual = function(actual, expected) {
+  let formatExpected = ' ';
+  let formatActual = ' ';
+
+  if (typeof actual === 'string') {
+    formatActual = `"${actual}"`;
+  } else {
+    formatActual = `${actual}`;
+  }
+  if (typeof expected === 'string') {
+    formatExpected = `"${expected}"`;
+  } else {
+    formatExpected = `${expected}`;
+  }
+
+  if (formatActual === formatExpected) {
+    console.log(`✅✅✅ Assertion Passed: ${formatActual} === ${formatExpected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${formatActual} !== ${formatExpected}`);
+  }
+};
+
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+
+  // scan over the entire list
+  for (const item of allItems) {
+    if (itemsToCount[item] === true) {
+      if (item in results) {
+        // results[item]
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+
+  return results;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
