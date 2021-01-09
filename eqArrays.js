5fbc2de1b7baf60b40937c6a39104607f6a009e8
dantan123 <dtan9988@gmail.com>
2021-01-09 05:14:03 +0000

    update eqArrays to support nested array comparisons

--- eqArrays.js
+++ eqArrays.js
@@ -28,8 +28,15 @@ const eqArrays = function(array1, array2) {
   }
   for (let i = 0; i < array1.length; i++) {
     // comparing by types
-    if (array1[i] !== array2[i]) {
-      return false;
+    if (Array.isArray(array1[i]) && Array.isArray(array2[i])) {
+      if (!eqArrays(array1[i], array2[i])) {
+        return false;
+      }
+    } else {
+      // items are not arrays
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
     }
   }
   return true;
@@ -41,3 +48,8 @@ assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([4, 'tiger', 'lion'], [4, 'tiger', 'lion']), true);
 assertEqual(eqArrays([true, null], [true, null]), true);
 assertEqual(eqArrays([undefined], [null]), false);
+
+// nest arrays
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
