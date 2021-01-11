47d352d1b2fbacaf47d7fd9870228aba1af422c5
dantan123 <dtan9988@gmail.com>
2021-01-11 22:28:47 +0000

    use M&C for headTest

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,24 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const head = require('../head');
 
-assertEqual(head([3,5,9]), 3);
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe("head", () => {
+  it("returns 1 for [1,2,3]", () => {
+    assert.strictEqual(head([1,2,3]), 1);
+  });
+
+  it("returns 5 for [5]", () => {
+    assert.strictEqual(head([5]), 5);
+  });
+
+  it("returns 3 for [3,5,9]", () => {
+    assert.strictEqual(head([3,5,9]), 3);
+  });
+
+  it("returns 'Hello' for ['Hello','Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello','Lighthouse', 'Labs']), 'Hello');
+  });
+
+  it("returns undefined for an empty array", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+})
